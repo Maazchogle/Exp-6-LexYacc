9f4d70f53e764788c071c0a9b2dead77b64e4617
Maazchogle <110540925+Maazchogle@users.noreply.github.com>
2024-04-28 16:51:51 +0530

    Add files via upload

---
+++ y.tab.h
@@ -0,0 +1,39 @@
+/*
+Name: Maaz Aslam Chogle                                               Batch: 1
+Roll no.: 21CO19                                                   Sem: VI
+*/
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    NUMBER = 258
+  };
+#endif
+/* Tokens.  */
+#define NUMBER 258
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
